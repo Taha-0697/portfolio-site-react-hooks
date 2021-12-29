129f3f95d3c82811d05fd14866909eae8055ddf4
Taha <chaghtai5.tc@gmail.com>
2021-12-29 21:21:32 +0500

    about component comppleted using flex-box in css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-//\ import Intro from "./components/flex-box/intro/Intro";
-import Intro from "./components/grid-box/intro/Intro";
+import About from "./components/flex-box/about/About";
+import Intro from "./components/flex-box/intro/Intro";
+//import Intro from "./components/grid-box/intro/Intro";
 
 const App = () => {
     return (
         <div>
             <Intro />
+            <About />
         </div>
     );
 };
