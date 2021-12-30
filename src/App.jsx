b972752e16492612fabcdce3c9300e1a3a2339dd
Taha-0697 <chaghtai5.tc@gmail.com>
2021-12-30 20:32:04 +0500

    Products Dynamically Added Portfolio Page Created name as Product

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-//import Intro from "./components/flex-box/intro/Intro";
-//import About from "./components/flex-box/about/About";
-import About from "./components/grid-box/about/About";
-import Intro from "./components/grid-box/intro/Intro";
+import Intro from "./components/flex-box/intro/Intro";
+import About from "./components/flex-box/about/About";
+import ProductList from './components/flex-box/productList/ProductList';
+
+//import About from "./components/grid-box/about/About";
+//import Intro from "./components/grid-box/intro/Intro";
 
 const App = () => {
     return (
         <div>
             <Intro />
             <About/>
+            <ProductList/>
         </div>
     );
 };
