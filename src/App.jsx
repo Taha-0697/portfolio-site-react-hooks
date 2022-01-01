0da80ab7686febab960ea37a30088f21fb8e2e1b
Taha <chaghtai5.tc@gmail.com>
2022-01-01 16:33:43 +0500

    contact page created and responsive done with email

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import Intro from "./components/flex-box/intro/Intro";
-import About from "./components/flex-box/about/About";
-import ProductList from './components/flex-box/productList/ProductList';
-import Contact from "./components/flex-box/contact/Contact";
+// FLex-box components imports
+// import Intro from "./components/flex-box/intro/Intro";
+// import About from "./components/flex-box/about/About";
+// import ProductList from './components/flex-box/productList/ProductList';
+// import Contact from "./components/flex-box/contact/Contact";
 
-
-
-// import About from "./components/grid-box/about/About";
-// import Intro from "./components/grid-box/intro/Intro";
-// import ProductList from './components/grid-box/productList/ProductList';
+// grid-box components imports
+import About from "./components/grid-box/about/About";
+import Intro from "./components/grid-box/intro/Intro";
+import ProductList from './components/grid-box/productList/ProductList';
+import Contact from './components/grid-box/contact/Contact';
 
 const App = () => {
     return (
